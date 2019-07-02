030409612de539c527d27bd39084bae19d041c9b
jamieberrier <berrier.jamie@gmail.com>
2019-07-02 19:24:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
 
     if @operation == 'add'
-      @number1 + @number2
+      (@number1 + @number2).to_s
     elsif @operation == 'subtract'
       @number1 - @number2
     elsif @operation == 'multiply'
