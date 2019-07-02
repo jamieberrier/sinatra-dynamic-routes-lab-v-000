a0c1ce9c7562e12262fcc6ffe33af6a62727b3b3
jamieberrier <berrier.jamie@gmail.com>
2019-07-02 19:27:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,10 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
 
-    
+    repeat = []
+    @number.times do
+      repeat << @phrase
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
